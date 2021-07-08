b7edfe59762717f7b9d51fbbd1e87d2a57974df0
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2021-07-08 10:30:38 -0700

    WIP

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-require('dotenv')
+require('dotenv').config()
+
 const express = require('express')
+
 const server = express()
-const PORT = process.env.PORT || 5000
-server.listen(PORT, () => {
-    console.log(`Listening at ${PORT}`)
-})
+
+server.use(express.json())
+
+if (process.env.NODE_ENV === 'development') {
+    const cors = require('cors')
+    server.use(cors())
+}
