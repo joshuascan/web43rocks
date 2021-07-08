aff2a8f04582ffadca1cf9dd4a778a24cdce9306
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2021-07-08 12:33:53 -0700

    WIP

--- index.js
+++ index.js
@@ -12,11 +12,13 @@ if (process.env.NODE_ENV === 'development') {
     server.use(cors())
 }
 
+// our API comes earlier in the pipeline
 server.get('/api/hello', (req, res) => {
     res.json({message: 'hello'})
 })
 
-server.use('*', (req, res) => {
+// catch-all that just sends back index.html
+server.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
 })
 
